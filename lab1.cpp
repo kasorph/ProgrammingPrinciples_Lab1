eadc3a58d61d7a98e90feee0b73c11fa81338bf8
Yerbol Kalikhan <kalikhane@gmail.com>
2025-06-11 03:02:33 +0500

    Update lab1.cpp with final version

--- lab1.cpp
+++ lab1.cpp
@@ -3,54 +3,54 @@ using namespace std;
 
 int main() {
     string name;
-    cout << "Enter your name: ";
+    cout << "Enter your name: " << endl;
     cin >> name;
     cout << "Hello, " << name << "!" << endl;
 
     int a, b;
-    cout << "\nEnter two integers: ";
+    cout << endl << "Enter two integers: " << endl;
     cin >> a >> b;
     cout << "The sum is " << a + b << endl;
 
     float radius;
-    cout << "\nEnter the radius of a circle: ";
+    cout << endl << "Enter the radius of a circle: " << endl;
     cin >> radius;
     cout << "The area is " << 3.14 * radius * radius << endl;
 
     int age;
-    cout << "\nEnter your age in years: ";
+    cout << endl << "Enter your age in years: " << endl;
     cin >> age;
     cout << "You are " << age * 12 << " months old." << endl;
 
     float celsius;
-    cout << "\nEnter temperature in Celsius: ";
+    cout << endl << "Enter temperature in Celsius: " << endl;
     cin >> celsius;
     cout << "Temperature in Fahrenheit: " << (celsius * 9/5) + 32 << endl;
 
     int num;
-    cout << "\nEnter an integer: ";
+    cout << endl << "Enter an integer: " << endl;
     cin >> num;
     cout << "The square is " << num * num << endl;
 
     float principal, rate, time;
-    cout << "\nEnter principal, rate, and time: ";
+    cout << endl << "Enter principal, rate, and time: " << endl;
     cin >> principal >> rate >> time;
     cout << "Simple Interest is " << (principal * rate * time) / 100 << endl;
 
     float weight, height;
-    cout << "\nEnter weight (kg) and height (m): ";
+    cout << endl << "Enter weight (kg) and height (m): " << endl;
     cin >> weight >> height;
     cout << "Your BMI is " << weight / (height * height) << endl;
 
     float n1, n2, n3;
-    cout << "\nEnter three numbers: ";
+    cout << endl << "Enter three numbers: " << endl;
     cin >> n1 >> n2 >> n3;
     cout << "The average is " << (n1 + n2 + n3) / 3 << endl;
 
-    int check;
-    cout << "\nEnter an integer to check even or odd: ";
-    cin >> check;
-    if (check % 2 == 0)
+    int somenum;
+    cout << endl << "Enter an integer to check even or odd: " << endl;
+    cin >> somenum;
+    if (somenum % 2 == 0)
         cout << "The number is even." << endl;
     else
         cout << "The number is odd." << endl;
